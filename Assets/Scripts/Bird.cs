92532fe94931ff4ebb4a76ec313b23a7bda8bde6
Ahmed Zaghloul <ahmed.mostafa.zag@gmail.com>
2024-05-03 22:44:42 +0300

    First Commit

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    [SerializeField] float _launchForce = 500;
+    [SerializeField] float _maxDragDistance = 5;
+    [SerializeField] Sprite _deadSprite;
+
+
+
+    Vector2 _startPosition;
+    Rigidbody2D _rigidbody2D;
+    SpriteRenderer _spriteRenderer;
+    
+     void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _startPosition = _rigidbody2D.position;
+        _rigidbody2D.isKinematic=true;
+    }
+     void OnMouseDown()
+    {
+        _spriteRenderer.color = Color.red;
+    }
+    void OnMouseUp()
+    {
+        var currentPosition = _rigidbody2D.position;
+        var direction = _startPosition - currentPosition;
+        direction.Normalize();
+        _rigidbody2D.isKinematic = false;
+        _rigidbody2D.AddForce(direction * _launchForce);
+
+
+
+
+        _spriteRenderer.color = Color.white;
+    }
+    private void OnMouseDrag()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector2 desiredPosition = mousePosition;
+            float distance = Vector2.Distance(desiredPosition, _startPosition);
+            if(distance > _maxDragDistance)
+            {
+                Vector2 direction = desiredPosition - _startPosition;
+                direction.Normalize();
+                desiredPosition = _startPosition + (direction * _maxDragDistance);
+
+
+            }
+        if (desiredPosition.x > _startPosition.x)
+        {
+            desiredPosition.x = _startPosition.x;
+        }
+
+        _rigidbody2D.position = desiredPosition;
+    }
+    // Update is called once per frame
+    void Update()
+    {   
+    }
+     void OnCollisionEnter2D(Collision2D collision)
+    {
+        GetComponent<SpriteRenderer>().sprite = _deadSprite;
+
+        StartCoroutine(ResetAfterDelay());
+        
+    }
+
+    IEnumerator ResetAfterDelay()
+    {
+        GetComponent<SpriteRenderer>().sprite = _deadSprite;
+
+        yield return new WaitForSeconds(2);
+        _rigidbody2D.position = _startPosition;
+        _rigidbody2D.isKinematic = true;
+        _rigidbody2D.velocity = Vector2.zero;
+    }
+}
---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SelectionBase]
+public class Monster : MonoBehaviour
+{
+    [SerializeField] Sprite _deadSprite;
+    [SerializeField] ParticleSystem _particleSystem;
+    private bool _hasDied;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (ShouldDieFromCollision(collision))
+        {
+
+            StartCoroutine(Die());
+
+        }
+    }
+
+    private bool ShouldDieFromCollision(Collision2D collision)
+    {
+        if (_hasDied)
+            return false;
+        Bird bird = collision.gameObject.GetComponent<Bird>();
+        if (bird != null)
+        {
+            return true;
+        }
+        if (collision.contacts[0].normal.y<-0.5)
+        {
+            return true;
+        }
+
+        else return false;
+    }
+
+    IEnumerator Die()
+    {
+        _hasDied = true;
+        GetComponent<SpriteRenderer>().sprite = _deadSprite;      
+        _particleSystem.Play();
+        yield return new WaitForSeconds(1);
+        gameObject.SetActive(false);
+    }
+}
